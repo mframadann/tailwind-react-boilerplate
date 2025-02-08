b3c5f6cb01c03feb243ca6e7288b1798c641ac58
Muhamad Firly Ramadan <ramadan@Ramadan-Mac.local>
2025-02-08 16:49:22 +0700

    try push

--- src/components/atoms/card.jsx
+++ src/components/atoms/card.jsx
@@ -1,7 +1,8 @@
 export const Card = ({ image }) => {
   return (
-    <div className='aspect-video max-w-2xl overflow-hidden p-4 shadow-lg rounded-2xl'>
+    <div className=' max-w-2xl overflow-hidden p-4 shadow-lg rounded-2xl'>
       <img src={image} alt='' className='w-full h-full rounded-3xl' />
+      <h1 className='text-[#151515] text-2xl leading-14'>Halo</h1>
     </div>
   );
 };
